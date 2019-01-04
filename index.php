a46d126b1c2fda95c66744a14c1885fcf5df1e21
ml lombry <webmaster@resinence.fr>
2019-01-04 12:30:10 +0100

    function checkdir added

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 $container="";
-$currentFile = "ORDER_20181227-0825.txt";
-$file = fopen($currentFile,'r');
-if (!file_exists($currentFile)) {
-	die("File does not exist");
-}
-
 function entcheck($lines){
 	$code=null;
 	$content='<table style="width:100%;border-collapse:collapse;"><thead><tr style="border:2px solid #ddd;"><th>ENT</th><th>NBLIG</th></tr></thead><tbody>';
@@ -30,28 +24,30 @@ function entcheck($lines){
 }
 
 //echo $container;
-
+function checkdir($dir="./"){
 $dir = "./";
+$container="";
 //  si le dossier pointe existe
-if (is_dir($dir)) {
+	if (is_dir($dir)) {
 
-   // si il contient quelque chose
-   if ($dh = opendir($dir)) {
+	   // si il contient quelque chose
+	   if ($dh = opendir($dir)) {
 
-       // boucler tant que quelque chose est trouve
-       while (($file = readdir($dh)) !== false) {
-       		if($file !=='index.php' && $file !=='.' && $file !=='..'){
-       			var_dump($file);
-       			$file = file($file);
-       			$container.=entcheck($file);
-       			 //echo "fichier : $file : type : " . filetype($dir . $file) . "<br />\n";
-       		}
-           // affiche le nom et le type
-          
-       }
-       // on ferme la connection
-       closedir($dh);
-   }
+	       // boucler tant que quelque chose est trouve
+	       while (($file = readdir($dh)) !== false) {
+	       		if($file !=='index.php' && $file !=='.' && $file !=='..' && $file !=='.git'){
+	       			$file = file($file);
+	       			$container.=entcheck($file);
+	       			 //echo "fichier : $file : type : " . filetype($dir . $file) . "<br />\n";
+	       		}
+	           // affiche le nom et le type
+	          
+	       }
+	       // on ferme la connection
+	       closedir($dh);
+	   }
+	}
+	return $container;
 }
 
-echo $container;
+echo checkdir("./");
